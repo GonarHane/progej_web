39e98d1bfb39bfc2b34962a5f80afe34c575c5c1
gonarhane <gonar90@gmail.com>
2022-08-30 08:57:43 +0000

    mardi

--- script.js
+++ script.js
@@ -41,50 +41,8 @@ submitBtn.addEventListener('click', (e)=> {
 infos.push(donnee)
 console.table(infos) 
 
-/* const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-/* const submitBtn = document.getElementById("submit"); */
-/* submitBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (
-    (data.nom && data.prenom && data.email && data.message) ||
-    regexEmail.test(data.email)
-  ) {
-    submitBtn.disabled = false;
-  } else {
-    submitBtn.disabled = true;
-  }
-  infos.push(data);
-  const result = document.getElementById("submit");
-  resetInputs();
-  resultat.innerHTML = `
-    <p>prenom: ${data.prenom}</p>
-    <p>nom: ${data.nom}</p>
-    <p>mail: ${data.email}</p>
-    <p>message: ${data.message}</p>
-    <p>message: ${data.telephone}</p>
-    `;
-  console.log(infos);
-}); */ 
-/* let inputs = document.getElementsByClassName("form");
-const data = {};
-const infos = []; 
-let inputsLength = inputs.length;
-for (let i = 0; i < inputsLength; i++) {
-  inputs[i].addEventListener("keyup", function () {
-    const id = inputs[i].id;
-    const value = inputs[i].value;
-    data[id] = value;
-  });
-}
-
-const resetInputs = () => {
-  for (let i = 0; i < inputsLength; i++) {
-    inputs[i].value = "";
-  }
-}; */
-//regex for email
-const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+/* const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; */
 
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
